6030dc1fd7a948b9b9e9925568e95f22421c3279
Vici <ripsodi@gmail.com>
2019-02-12 20:53:14 +0300

    Add User model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful"""
+        """Test creating a new user with an email is successful."""
         email = "nort87@mail.ru"
         password = "sugarbooger123"
         user = get_user_model().objects.create_user(email=email, password=password)
@@ -12,3 +12,23 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized."""
+        email = "petrovich@IBM.COM"
+        user = get_user_model().objects.create_user(email, "testpass123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser."""
+        user = get_user_model().objects.create_superuser(
+            "test@allthegoodies.com", "test343434"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
